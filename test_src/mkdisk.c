ecc24408dbc32b07967a110e0244b67458e86619
anastasia578 <an738@georgetown.edu>
2021-05-07 01:48:51 +0000

    mkdisk

--- test_src/mkdisk.c
+++ test_src/mkdisk.c
@@ -5,13 +5,7 @@
 //create a disk (MYFS) in dev/shm
 int main() {
 	printf("Making disk\n");
-<<<<<<< HEAD
-	//pdos_mkdisk(10);
-	printf("Made disk\n");
-	printf("Hello");
-=======
 	pdos_mkdisk(10);
 	printf("Made disk\n");
->>>>>>> 6291e7e013670ea113ce46d0e65e5988045a63a3
 	return 0;
 }
