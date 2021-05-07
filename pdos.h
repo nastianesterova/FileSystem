ba5eeb47446f9b633899438b6a11884a9f15a08c
anastasia578 <an738@georgetown.edu>
2021-05-07 03:00:07 +0000

    changed from #pragma once to old style

--- pdos.h
+++ pdos.h
@@ -1,11 +1,15 @@
-#pragma once
-typedef void PDOS_FILE;
+#ifndef PDOS_H
+#define PDOS_H
+
+#include "pdosfilesys.h"
 
 void pdos_mkdisk(int sz);
 void pdos_mkfs(char * ID);
 PDOS_FILE *pdos_open(const char *fname, const char *mode);
-int pdos_fgetc(PDOS_FILE *);
-void pdos_fputc(int b, PDOS_FILE*);
-void pdos_fclose(PDOS_FILE *);
+int pdos_fgetc(PDOS_FILE * file);
+void pdos_fputc(int b, PDOS_FILE* file);
+void pdos_fclose(PDOS_FILE * file);
 char **pdos_dir();
 void pdos_mkdir(char *dir);
+
+#endif
