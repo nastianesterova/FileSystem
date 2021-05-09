19df0e7c948339e1467ed201541a1b3b187b5a7b
Anastasia <nastia@homer01.alpha.home>
2021-05-09 18:41:21 -0400

    still working on pdos_open

--- pdos_open.c
+++ pdos_open.c
@@ -4,6 +4,7 @@ PDOS_FILE *pdos_open(const char *fname, const char *mode) {
     printf("max directory entries: %ld\n", MAX_NUM_DIRECTORIES_ENTRIES);
     _pdos_open_fs();
     // open fs, look for fname in directory block pdos_fs + 3
+    // need to initialize block to something
     DISK_BLOCK* block;
     _pdos_read_block(block, 3);
 
