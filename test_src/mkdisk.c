bfe3d6779157f6be10bb2d474a66098b4d68da80
anastasia578 <an738@georgetown.edu>
2021-05-17 16:21:57 +0000

    update

--- test_src/mkdisk.c
+++ test_src/mkdisk.c
@@ -7,7 +7,8 @@ int main() {
 	long sz = BLOCK_SIZE * MAXBLOCKS;
 	printf("Making disk of size %ld\n", sz);
 	pdos_mkdisk(sz);
-	struct stat buffer;
+	struct stat buffer; //creating a stat buffer to be able
+	// to access the file size
 	int fd = open(MYFS, O_RDONLY);
 	fstat(fd, &buffer);
 	printf("Size of MYFS disk: %ld\n", buffer.st_size);
